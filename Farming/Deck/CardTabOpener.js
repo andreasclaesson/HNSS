958f622db1f9b94ae4184447ebf16bb4d536f5be
Andreas <hello@venix.me>
2023-04-27 17:27:01 +0200

    Add CardTabOpener v0.1

---
+++ Farming/Deck/CardTabOpener.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         CardTabOpener
+// @version      0.1
+// @namespace    HNSS
+// @description  Opens card in new tab when clicked on it
+// @downloadURL  https://raw.githubusercontent.com/venixdeveloper/HNSS/main/Farming/Deck/CardTabOpener.js
+// @updateURL    https://raw.githubusercontent.com/venixdeveloper/HNSS/main/Farming/Deck/CardTabOpener.js
+// @author       VenixDeveloper (Vulxo)
+// @noframes
+// @match        https://www.nationstates.net/page=deck
+// @grant        GM_openInTab
+// ==/UserScript==
+(function () {
+    'use strict';
+    let dcc = document.querySelectorAll('.deckcard-container');
+
+    for (let i=0; i < dcc.length; i++) {
+        let cardId = dcc[i].getElementsByClassName("deckcard")[0].getAttribute("data-cardid");
+        let season = dcc[i].getElementsByClassName("deckcard")[0].getAttribute("data-season");
+
+        // Remove black popup box
+        dcc[i].getElementsByClassName("deckcard-info")[0].remove();
+        // Add onclick attribute
+        dcc[i].setAttribute("onclick", `window.open('https://www.nationstates.net/page=deck/card=${cardId}/season=${season}', '_blank');`);
+    }
+})();

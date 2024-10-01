96951ac6b9aaa1d1733e9a1b6bbe4569e9d5e692
Venix <andcla07@edu.gislaved.se>
2024-10-01 23:06:19 +0200

    Update CardSharksAssist.user.js

--- Farming/Deck/CardSharksAssist.user.js
+++ Farming/Deck/CardSharksAssist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CardSharksAssist
 // @namespace    HNSS
-// @version      0.8.2
+// @version      0.9.0
 // @description  A script that makes gifting cards included within the Card Sharks much easier, also prevents junking them
 // @downloadURL  https://raw.githubusercontent.com/andreasclaesson/HNSS/main/Farming/Deck/CardSharksAssist.user.js
 // @updateURL    https://raw.githubusercontent.com/andreasclaesson/HNSS/main/Farming/Deck/CardSharksAssist.user.js
@@ -20,6 +20,7 @@
 document.addEventListener('DOMContentLoaded', async function() {
     'use strict';
 
+    // Get stored data and create button
     let puppetArray = getStoredData() || [];
     GM.registerMenuCommand('Refresh Data from Google Spreadsheet', async function () {
         try {
@@ -33,6 +34,8 @@ document.addEventListener('DOMContentLoaded', async function() {
         }
     });
 
+    // If there's nothing in the stored data, fetch it
+    // Else, go on with script
     if (!puppetArray.length) {
         getSpreadsheetData()
             .then(data => {
@@ -48,23 +51,31 @@ document.addEventListener('DOMContentLoaded', async function() {
         processPage();
     }
     function processPage() {
+        // If the URL contains /gift=1 (user is on card's gift page)
         if (window.location.href.indexOf("/gift=1") > -1) {
+            // Get attributes of the card
             let cardName = document.getElementsByClassName("nname")[0].innerHTML;
-            let cardId = document.getElementById("deck-single-card").getAttribute('data-cardid');
+            let cardId = document.getElementById("deck-single-card").getAttribute('data-cardid'); 
 
+            // Get info on who the card should be gifted to
             let card = findId(puppetArray, cardId)
             if (card) {
+                // and apply it
                 document.getElementById("entity_name").value = card.gift;
                 document.getElementsByName("send_gift")[0].focus();
             }
+        // .. and so on (user is on the deck page)
         } else if (window.location.href.indexOf("/page=deck") > -1) {
+            // get all cards junk button
             let junkButtons = document.querySelectorAll(
                 'a.deckcard-junk-button[data-rarity="common"],a.deckcard-junk-button[data-rarity="uncommon"], a.deckcard-junk-button[data-rarity="rare"], a.deckcard-junk-button[data-rarity="ultra-rare"],a.deckcard-junk-button[data-rarity="epic"]'
             );
             let convertedJunk = [...junkButtons];
+            // loop through em and remove junk button as well as Card Sharks Assist "watermark" *wink*
             convertedJunk.forEach(button => {
                 let card = findId(puppetArray, button.getAttribute('data-cardid'));
                 if (card && card.id) {
+                    button.setAttribute('data-csa', 'true'); // can be used for other scripts, e.g. "junk confirmation changer" script to know that it should be skipped.
                     button.classList.remove("deckcard-junk-button");
                 }
             });
